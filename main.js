1b8c207f74ca74627a058e8dc2d8910e0c8f4fc6
atenziono <atenziono@gmail.com>
2023-11-18 14:45:17 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow(
+    {
+      title:'Electron',
+      width:'1000',
+      height:'600'
+    }
+  );
+
+mainWindow.webContents.openDevTools();
+
+  const startUrl = url.format({
+    pathname:path.join(__dirname,'./app/build/index.html'),
+    protocol:'file'
+  });
+
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(createMainWindow);
